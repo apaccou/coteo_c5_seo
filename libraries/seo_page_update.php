1c2f0634ed39176fb9e615b9a23495e7b4fc915a
Alexandre PACCOU <a.paccou@coteo.com>
2016-08-02 17:42:29 +0200

    Formatage htmlentities de toutes les sorties de fonction de type getPublic...

--- libraries/seo_page_update.php
+++ libraries/seo_page_update.php
@@ -27,9 +27,7 @@ class SeoPageUpdate
     $this->newKeywords = $pageKeywords;
 
     $this->oldName = $this->getPublicPageName();
-    // Todo : à vérifier / compléter
     $this->oldTitle = $this->getPublicPageTitle();
-    // Todo : à vérifier / compléter
     $this->oldDescription = $this->getPublicPageDescription();
     $this->oldKeywords = $this->getPublicPageKeywords();
     $this->url = $this->getPublicPageUrl();
@@ -45,20 +43,20 @@ class SeoPageUpdate
   public function getPublicPageTitle()
   {
     $pageTitle = $this->_cobj->getCollectionName();
-    $pageTitle = htmlspecialchars($pageTitle, ENT_COMPAT, APP_CHARSET);
     $autoTitle = sprintf(PAGE_TITLE_FORMAT, SITE, $pageTitle);
     $pageTitle = $this->_cobj->getAttribute('meta_title') ? $this->_cobj->getAttribute('meta_title') : $autoTitle;
+    $pageTitle = htmlspecialchars($pageTitle, ENT_COMPAT, APP_CHARSET);
     return $pageTitle;
   }
 
   public function getPublicPageDescription()
   {
-    $pageDescription = $this->_cobj->getCollectionDescription();
-    $autoDesc = htmlspecialchars($pageDescription, ENT_COMPAT, APP_CHARSET);
+    $autoDesc = $this->_cobj->getCollectionDescription();
     $pageDescription = $this->_cobj->getAttribute('meta_description') ? $this->_cobj->getAttribute('meta_description') : $autoDesc;
     // Todo : Vérifier si nécessaire pour le XML
 $pageDescription = str_replace("\n","",$pageDescription);
 $pageDescription = str_replace("\r","",$pageDescription);
+    $pageDescription = htmlspecialchars($pageDescription, ENT_COMPAT, APP_CHARSET);
     return $pageDescription;
   }
 
