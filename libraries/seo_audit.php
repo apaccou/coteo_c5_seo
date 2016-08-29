a081cc8d0b318221baad162884b6ba7c4b3bf8c7
Alexandre PACCOU <a.paccou@coteo.com>
2016-08-29 10:02:44 +0200

    Avancées du week-end

--- libraries/seo_audit.php
+++ libraries/seo_audit.php
@@ -1,39 +1,57 @@
 <?php
 
-// Todo : classe à construire
-// Important : Séparer éléments propres à Concrete5 du reste
-// Todo : voir si possibilité de require HttpRequest pour s'assurer de son chargement
+// Todo : Important : Séparer éléments propres à Concrete5 du reste
+require('http_request.class.php');
 
 class SeoAudit
 {
   public $startingHttpRequest;
+  public $maxUrlsToScan = 20;
   private $_scanDepth;
-  public $analysedPagesCount;
+  public static $analysedPagesCount;
   private $_urlsToScan = array();
   private $_urlsScanned = array();
 
-  // Récup URL Page ==> Analayse de la page
-  // Récup URL ACCUEIL ==> Récupération des URLs du SITE ==> Analyse de la page
+  private $_crawlUrlCodeError = array();
+  private $_crawlLinkInternalError = array();
+  private $_crawlLinkExternalError = array();
+
+  private $_externalLinkError = array();
 
   public function __construct($url , $scanDepth = 0)
   {
     $this->_scanDepth = $scanDepth;
     $this->analysedPagesCount = 0;
 
+    // Todo : vérifier si impact d'analyser l'url avec ou sans / à la fin au départ
+    if(substr($url, -1, 1) != '/') {
+      $url = $url . '/';
+    }
+
     $this->startingHttpRequest = new HTTPRequest($url);
+    // évite le scan de http://localhost/ lorsqu'un domaine ou sous-domaine pointant vers la racine des fichiers du site n'est pas définit
+    if($this->startingHttpRequest->_host == 'localhost') {
+      $this->_urlsScanned[] = 'http://localhost/';
+    }
     $this->addUrlToScan($url);
   }
 
   public function addUrlToScan($url)
   {
-    $url = $this->rel2abs($url, $this->_host);
-    if(!in_array($url, $this->_urlsToScan)) {
+    // Todo : vérifier comportement avec Anchor sur Url Externe
+    $url = $this->getAbsoluteLink($url, $this->_host);
+    if(!in_array($url, $this->_urlsToScan) && !in_array($url, $this->_urlsScanned) ) {
       $this->_urlsToScan[] = $url;
     }
   }
 
   public function isExternalLink($url)
   {
+    if($this->isAnchor($url)) {
+      $parsedUrl = parse_url($url);
+      // Suppresion de l'ancre
+      $url = $parsedUrl['scheme'] . '://' . $parsedUrl['host'] . $parsedUrl['path'] . $parsedUrl['query'];
+    }
     $r = new HttpRequest($url);
     if($r->_host == $this->startingHttpRequest->_host) {
       return false;
@@ -42,17 +60,53 @@ class SeoAudit
     }
   }
 
+  public function isAnchor($url) {
+    // Todo : voir si plus rapide avec http://php.net/manual/fr/function.parse-url.php
+    if(strpos($url,"#") === false) {
+      return false;
+    } else {
+      // echo "ANCRE  : #".explode( "#", $url )[1] . '<br/>';
+      return true;
+    }
+  }
+
   public function analysePageAll()
   {
     foreach ($this->_urlsToScan as $key=>$url) {
-      echo '<h2>'. __FUNCTION__ . '</h2>';
-      echo 'analyse en cours de la page : ' . $url . '<br/>';
+      echo '<h2>Boucle '. __FUNCTION__ . '</h2>';
+      echo 'analyse en cours de la page : ' . $key . ' / '. $url . '<br/>';
       // Limite
-      if($this->analysedPagesCount >= 20) {
+      if($this->analysedPagesCount == $this->maxUrlsToScan) {
+        echo '<p>Limite maxUrlsToScan atteinte</p>';
+        break;
+      }
+      // Vérifications
+      if(in_array($url, $this->_urlsScanned)) {
+        echo '<p>Url déjà scannée</p>';
         continue;
       }
       // Traitements
       $this->fetchUrl($url);
+      $pageLinks = $this->getPageLinks($url);
+      echo '<h2>Traitements '. __FUNCTION__ . '</h2>';
+      // Boucle pour ajout des nouvelles urls à analyser
+      foreach ($pageLinks as $link) {
+        if($this->isExternalLink($link)) {
+          echo 'Externe ==> ' . $link . '<br/>';
+          // Todo : vérifier linkChecker
+          // Todo : checker toutes les urls externe une seule fois à la fin pour éviter doublon d'analyse
+          $this->checkExternalLinkError($link);
+        } else {
+          echo 'Interne ==> ' . $link . '<br/>';
+          // Todo : vérifier que l'ajout se fasse bien pour les nouvelles URL qui contiennent une ancre
+          if(!$this->isAnchor($link)) {
+            $this->addUrlToScan($link);
+          }
+        }
+        echo '<hr/>';
+      }
+
+echo '<br/>';
       // Mise à jour des variables
       if(!in_array($url, $this->_urlsScanned)) {
         $this->_urlsScanned[] = $url;
@@ -63,30 +117,72 @@ class SeoAudit
       // Pause pour éviter saturation du serveur et de se faire bannir par fail2ban
       // Todo : pause sur les urls externes
       if($this->_host != 'localhost') {
-        sleep(1);
+        //sleep(1);
       }
 
-      // Boucle pour ajout des nouvelles urls à analyser
-      // Todo : limiter aux urls internes
+      // Todo : limiter aux urls internes et optimiser
       if($this->isExternalLink($url)) {
-        // Todo :linckCkecker ici ?
-        echo '<p>Lancement linckCkecker</p>';
+        // rien
       } else {
-        if($this->_scanDepth = 1) {
-          $this->getPageLinks($url);
+        if($this->_scanDepth == 1) {
+          $this->analysePageAll();
         }
       }
     }
-    echo '<h2>'. __FUNCTION__ . '</h2>';
-    echo $this->analysedPagesCount;
+  }
+
+  public function reportAll() {
+    echo '<h2> Analyse '. __FUNCTION__ . '</h2>';
+    echo '<p>' . $this->analysedPagesCount . ' pages analysées</p>';
+    echo '<h3>_urlsToScan : ' . count($this->_urlsToScan). '</h3>';
     echo '<pre>';
     var_dump($this->_urlsToScan);
     echo '</pre>';
+    echo '<h3>_urlsScanned : ' . count($this->_urlsScanned) . '</h3>';
     echo '<pre>';
     var_dump($this->_urlsScanned);
     echo '</pre>';
+    $this->reportCrawlUrlCodeError();
+    $this->reportCrawlExternalLinkError();
   }
 
+  public function checkIsUrl($url) {
+    $pattern='|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i';
+  	if(preg_match($pattern, $url) > 0) return true;
+  	else return false;
+  }
+
+  // Check if URL exists and is Online
+  public function checkIsAccesible($url) {
+    $url = @parse_url($url);
+    if (!$url) return false;
+
+    $url = array_map('trim', $url);
+    $url['port'] = (!isset($url['port'])) ? 80 : (int)$url['port'];
+
+    $path = (isset($url['path'])) ? $url['path'] : '/';
+    $path .= (isset($url['query'])) ? "?$url[query]" : '';
+
+    if (isset($url['host']) && $url['host'] != gethostbyname($url['host'])) {
+
+      $fp = fsockopen($url['host'], $url['port'], $errno, $errstr, 30);
+
+      if (!$fp) return false; //socket not opened
+
+      fputs($fp, "HEAD $path HTTP/1.1\r\nHost: $url[host]\r\n\r\n"); //socket opened
+      $headers = fread($fp, 4096);
+      fclose($fp);
+
+      if(preg_match('#^HTTP/.*\s+[(200|301|302)]+\s#i', $headers)){//matching header
+        return true;
+      }
+      else return false;
+
+    } // if parse url
+    else return false;
+  }
+
+// Todo : comparer utiilité avec fonction : http://php.net/manual/fr/function.parse-url.php
   public function fetchUrl($url)
   {
     echo '<h2>'. __FUNCTION__ . '</h2>';
@@ -102,7 +198,7 @@ class SeoAudit
     echo '</pre>';
   }
 
-  public function test($url) {
+  public function testDOMDocument($url) {
     $file = $url;
     $doc = new DOMDocument();
     $doc->loadHTMLFile($file);
@@ -123,23 +219,26 @@ class SeoAudit
   public function getPageLinks($url) {
     $file = $url;
     echo '<h2>'. __FUNCTION__ . '</h2>';
-    echo 'analyse en cours de la page : ' . $url . '<br/>';
+    echo 'récupération de tous les liens de la page : ' . $url . '<br/>';
     $doc = new DOMDocument();
     $doc->loadHTMLFile($file);
 
+    // Todo : supprimer notes
     // Affichage du contenu texte de body
     // $elements = $doc->getElementsByTagName('body');
     // $plainText = $elements->textContent;
 
+    // Todo : supprimer sorties d'aides
+
     $links = $doc->getElementsByTagName('a');
-    echo '<h2>'. __FUNCTION__ . '</h2>';
     foreach ($links as $link) {
            echo 'Lien brut : ' . $linkHref = $link->getAttribute('href');
            echo '<br/>';
-           echo 'Lien absolu : ' . $linkHrefAbsolute = $this->rel2abs($linkHref, $this->startingHttpRequest->_protocol . '://' . $this->startingHttpRequest->_host);
+           echo 'Lien absolu : ' . $linkHrefAbsolute = $this->getAbsoluteLink($linkHref, $this->startingHttpRequest->_protocol . '://' . $this->startingHttpRequest->_host . '/');
            echo '<br/>';
-           $this->addUrlToScan($linkHrefAbsolute);
+           $pageLinks[] = $linkHrefAbsolute;
     }
+    return $pageLinks;
   }
 
   public function getHeaders($url)
@@ -174,6 +273,17 @@ class SeoAudit
   // Analyse du crawl //
   //////////////////////
 
+  public function crawlAnalyseAll($url) {
+    // liste des URL en erreur (codes 301, 302, 404, 410, 500, etc.)
+    $headers = $this->getHeaders($url);
+    $code = $this->getHttpResponseCode($headers);
+    // Todo : vérifier les codes à inscrire en erreur
+    if($this->checkHttpResponseCode($code)) {
+      $this->_crawlUrlCodeError[] = array($url, $code);
+    }
+    // liste des URL faisant un lien vers une URL en erreur
+  }
+
   public function getHttpResponseCode($headers) {
     $code = substr($headers[0], 9, 3);
     return $code;
@@ -184,54 +294,80 @@ class SeoAudit
     if ($code === NULL) {
       return false;
     }
-
+    // Todo : voir si $text à conserver ou supprimer
     switch ($code) {
-      case 100: $text = 'Continue'; break;
-      case 101: $text = 'Switching Protocols'; break;
-      case 200: $text = 'OK'; break;
-      case 201: $text = 'Created'; break;
-      case 202: $text = 'Accepted'; break;
-      case 203: $text = 'Non-Authoritative Information'; break;
-      case 204: $text = 'No Content'; break;
-      case 205: $text = 'Reset Content'; break;
-      case 206: $text = 'Partial Content'; break;
-      case 300: $text = 'Multiple Choices'; break;
-      case 301: $text = 'Moved Permanently'; break;
-      case 302: $text = 'Moved Temporarily'; break;
-      case 303: $text = 'See Other'; break;
-      case 304: $text = 'Not Modified'; break;
-      case 305: $text = 'Use Proxy'; break;
-      case 400: $text = 'Bad Request'; break;
-      case 401: $text = 'Unauthorized'; break;
-      case 402: $text = 'Payment Required'; break;
-      case 403: $text = 'Forbidden'; break;
-      case 404: $text = 'Not Found'; break;
-      case 405: $text = 'Method Not Allowed'; break;
-      case 406: $text = 'Not Acceptable'; break;
-      case 407: $text = 'Proxy Authentication Required'; break;
-      case 408: $text = 'Request Time-out'; break;
-      case 409: $text = 'Conflict'; break;
-      case 410: $text = 'Gone'; break;
-      case 411: $text = 'Length Required'; break;
-      case 412: $text = 'Precondition Failed'; break;
-      case 413: $text = 'Request Entity Too Large'; break;
-      case 414: $text = 'Request-URI Too Large'; break;
-      case 415: $text = 'Unsupported Media Type'; break;
-      case 500: $text = 'Internal Server Error'; break;
-      case 501: $text = 'Not Implemented'; break;
-      case 502: $text = 'Bad Gateway'; break;
-      case 503: $text = 'Service Unavailable'; break;
-      case 504: $text = 'Gateway Time-out'; break;
-      case 505: $text = 'HTTP Version not supported'; break;
+      case 100: $error = 1; $text = 'Continue'; break;
+      case 101: $error = 1; $text = 'Switching Protocols'; break;
+      case 200: $error = 1; $text = 'OK'; break;
+      case 201: $error = 1; $text = 'Created'; break;
+      case 202: $error = 1; $text = 'Accepted'; break;
+      case 203: $error = 1; $text = 'Non-Authoritative Information'; break;
+      case 204: $error = 1; $text = 'No Content'; break;
+      case 205: $error = 1; $text = 'Reset Content'; break;
+      case 206: $error = 1; $text = 'Partial Content'; break;
+      case 300: $error = 1; $text = 'Multiple Choices'; break;
+      case 301: $error = 0; $text = 'Moved Permanently'; break;
+      case 302: $error = 0; $text = 'Moved Temporarily'; break;
+      case 303: $error = 1; $text = 'See Other'; break;
+      case 304: $error = 1; $text = 'Not Modified'; break;
+      case 305: $error = 1; $text = 'Use Proxy'; break;
+      case 400: $error = 1; $text = 'Bad Request'; break;
+      case 401: $error = 1; $text = 'Unauthorized'; break;
+      case 402: $error = 1; $text = 'Payment Required'; break;
+      case 403: $error = 1; $text = 'Forbidden'; break;
+      case 404: $error = 0; $text = 'Not Found'; break;
+      case 405: $error = 1; $text = 'Method Not Allowed'; break;
+      case 406: $error = 1; $text = 'Not Acceptable'; break;
+      case 407: $error = 1; $text = 'Proxy Authentication Required'; break;
+      case 408: $error = 1; $text = 'Request Time-out'; break;
+      case 409: $error = 1; $text = 'Conflict'; break;
+      case 410: $error = 0; $text = 'Gone'; break;
+      case 411: $error = 1; $text = 'Length Required'; break;
+      case 412: $error = 1; $text = 'Precondition Failed'; break;
+      case 413: $error = 1; $text = 'Request Entity Too Large'; break;
+      case 414: $error = 1; $text = 'Request-URI Too Large'; break;
+      case 415: $error = 1; $text = 'Unsupported Media Type'; break;
+      case 500: $error = 0; $text = 'Internal Server Error'; break;
+      case 501: $error = 1; $text = 'Not Implemented'; break;
+      case 502: $error = 1; $text = 'Bad Gateway'; break;
+      case 503: $error = 1; $text = 'Service Unavailable'; break;
+      case 504: $error = 1; $text = 'Gateway Time-out'; break;
+      case 505: $error = 1; $text = 'HTTP Version not supported'; break;
       default:
       trigger_error('Unknown http status code ' . $code, E_USER_ERROR); // exit('Unknown http status code "' . htmlentities($code) . '"');
       return false;
     }
-    return $text;
+    return $error;
   }
 
+  public function reportCrawlUrlCodeError() {
+    // liste des URL en erreur (codes 301, 302, 404, 410, 500, etc.)
+    echo '<h2>'. __FUNCTION__ . '</h2>';
+    echo '<pre>';
+    var_dump($this->_crawlUrlCodeError);
+    echo '</pre>';
+  }
+
+  public function checkExternalLinkError($url) {
+      if(!$this->checkIsAccesible($url)) {
+        $this->_externalLinkError = array($url, $code);
+      }
+  }
+
+  public function reportCrawlExternalLinkError() {
+    // liste des URL faisant un lien vers une URL en erreur
+    echo '<h2>'. __FUNCTION__ . '</h2>';
+    echo '<pre>';
+    var_dump($this->_externalLinkError);
+    echo '</pre>';
+  }
+
+    //////////////////////
+    // Analyse d //
+    //////////////////////
+
   // Transfrom relative path into absolute URL using PHP
-  public function rel2abs($rel, $base)
+  public function getAbsoluteLink($rel, $base)
   {
       /* return if already absolute URL */
       if (parse_url($rel, PHP_URL_SCHEME) != '') return $rel;
--- single_pages/dashboard/coteo/audit/view.php
+++ single_pages/dashboard/coteo/audit/view.php
@@ -6,6 +6,7 @@ echo  Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('AUD
 <h1>Audit Technique SEO</h1>
 <p>Réalise un audit technique SEO d'un site à partir d'une URL.</p>
 <br/>
+
 <h2>Analyse du crawl</h2>
 <p>Todo : description à remplir.</p>
 <hr/>
@@ -13,24 +14,50 @@ echo  Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('AUD
 
 // Todo : formulaire de récupération de l'url de départ
 $url = 'http://localhost/concrete5634/';
+$url = 'http://www.tourisme-gravelines.fr';
 
 if($url) {
   // Todo : paramétrage d'une variable pour audit une page ou tout site 0 = une page / 1 = toutes pages
   $audit = new SeoAudit($url, 1);
+
+  if(!$audit->checkIsUrl($url)) {
+    echo 'Problème : veuillez fournir une url valide';
+    exit;
+  }
+  if(!$audit->checkIsAccesible($url)) {
+    echo 'Problème : le site n\'est pas accessible';
+    exit;
+  }
+Loader::library('timing_helper', 'coteo_c5_seo');
+$th = new TimingHelper();
+ $audit->analysePageAll();
+$time = $th->time();
+// $th->start();
+//   $audit->crawlAnalyseAll($url);
+//   echo '<br/>';
+// echo $th->time();
+$audit->reportAll();
+
+
   // Todo : Vérifier que le scan se fait bien comme si GoogleBot
+  echo '<ul>';
+  echo '<li>Site analysé : ' . $audit->startingHttpRequest->_url . '</li>';
+  echo '<li>URL de départ : ' . $audit->startingHttpRequest->_url . '</li>';
+  echo '<li>Temps d\'analyse : ' . $time .'</li>';
+  echo '</ul>';
 }
 
 // Analyse des entêtes
 
-$headers = $audit->getHeaders($url);
-$code = $audit->getHttpResponseCode($headers);
-// Todo : fonction checkHttpResponseCode et retours à compléter
-if($audit->checkHttpResponseCode($code)) {
-  // Todo : voir si possibilité de reprendre le contenu de getHeaders pour éviter des requêtes
-  $audit->getPageLinks($url);
-  $audit->test($url);
-  $audit->analysePageAll();
-}
+// $headers = $audit->getHeaders($url);
+// $code = $audit->getHttpResponseCode($headers);
+// // Todo : fonction checkHttpResponseCode et retours à compléter
+// if($audit->checkHttpResponseCode($code)) {
+//   // Todo : voir si possibilité de reprendre le contenu de getHeaders pour éviter des requêtes
+//   //$audit->getPageLinks($url);
+//   //$audit->testDOMDocument($url);
+//   $audit->analysePageAll();
+// }
 ?>
 <br/>
 <h2>Vitesse</h2>
